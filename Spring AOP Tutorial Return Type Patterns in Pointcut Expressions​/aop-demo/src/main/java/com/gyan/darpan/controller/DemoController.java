ea72ea9254a3f1062514b96eaf0ebf1d70dfbe4d
gyandarpan22 <158079373+gyandarpan22@users.noreply.github.com>
2026-04-14 22:41:06 +0530

    Add files via upload

---
+++ Spring AOP Tutorial Return Type Patterns in Pointcut Expressions​/aop-demo/src/main/java/com/gyan/darpan/controller/DemoController.java
@@ -0,0 +1,46 @@
+package com.gyan.darpan.controller;
+
+import com.gyan.darpan.dto.UserDto;
+import com.gyan.darpan.service.DemoService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo")
+@Log4j2
+public class DemoController {
+    private final DemoService demoService;
+    private final ModifierDemo modifierDemo;
+
+    public DemoController(DemoService demoService, ModifierDemo modifierDemo) {
+        this.demoService = demoService;
+        this.modifierDemo = modifierDemo;
+    }
+
+    @GetMapping
+    public String displayMessage(
+            @RequestParam("userName") String userName,
+            @RequestParam("age") int age,
+            @RequestParam("message") String message,
+            @RequestParam("count") int count
+    ) {
+        log.info("Inside DemoController:displayMessage() , demoService : {}", demoService.getClass());
+
+        demoService.createUser(new UserDto(userName, age), message, count);
+
+        return demoService.displayMessage(new UserDto(userName, age), message, count);
+    }
+
+    @GetMapping("modiferDemo")
+    public String modiferDemo() {
+        log.info("Inside DemoController:modiferDemo() , modifierDemo : {}", modifierDemo.getClass());
+
+        modifierDemo.displayMessage();
+        modifierDemo.protectedDisplayMessage();
+
+        return "Success";
+    }
+}
---
+++ Spring AOP Tutorial Return Type Patterns in Pointcut Expressions​/aop-demo/src/main/java/com/gyan/darpan/service/DemoService.java
@@ -0,0 +1,23 @@
+package com.gyan.darpan.service;
+
+import com.gyan.darpan.dto.UserDto;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class DemoService {
+
+    public String displayMessage(UserDto user, String message, int count) {
+        log.info("Inside DemoService:displayMessage()");
+
+        return String.format("User : %s , Message : %s , Count :%d", user.toString(), message, count);
+    }
+
+    public UserDto createUser(UserDto user, String message, int count) {
+        log.info("Inside DemoService:createUser()");
+
+        return user;
+    }
+
+}
